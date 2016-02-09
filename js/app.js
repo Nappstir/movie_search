35808926202cb3d3a907ccaa5774d4917db0bf58
Travis Siebenhaar <trsiebenhaar@gmail.com>
2016-02-08 21:55:59 -0700

    Add html partials & http.get request for movie data

---
+++ js/app.js
@@ -0,0 +1,23 @@
+angular.module('myApp', [])
+  .controller('MovieController', function($scope, $http){
+    $scope.$watch('search', function(){
+      fetch();
+    });
+
+    $scope.search = "Hot Rod"
+
+    function fetch(){
+      $http.get("http://omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+      .then(function(response){
+        $scope.details = response.data;
+      });
+
+      $http.get("http://omdbapi.com/?s=" + $scope.search)
+      .then(function(response){
+        $scope.related = response.data;
+        console.log(response)
+        console.log(response.data)
+      });
+    }
+
+  })
